c630f1cd209dbad6131df7869fefb60213728bb9
dauarda <141158585+dauarda@users.noreply.github.com>
2024-11-18 08:29:36 -0300

    Add files via upload

---
+++ flashcard 3tri/pergunta.js
@@ -0,0 +1,65 @@
+criaCartao(
+    ' Geografia',
+    'Qual é o maior deserto do mundo',
+    'deserto da Antartica'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro país a abolir a escravidão?',
+    'Haiti em 1804.'
+)
+
+criaCartao(
+    ' Cultura Geral',
+    'Quem escreveu o Dom Quixote?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'Cultura Geral',
+    'Em que ano foi lançado a primeira copa do Brasil?',
+    ' 1930.'
+)
+
+
+criaCartao(
+    'Português / Literatura',
+    ' Pai do Modernismo',
+    'Mário de Andrade'
+)
+
+
+criaCartao(
+    'Química',
+    'Que elemento tem o símbolo Fe?',
+    'ferro'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual o passado de GO?',
+    'Went'
+)
+
+
+criaCartao(
+    'matemática',
+    ' como calcular a porcentagem e divida ?',
+    ' Multiplica o número pela porcentagem e divide por 100'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+
+
+criaCartao(
+    'Biologia',
+    'Oque é célula"?',
+    'A célula é a menor unidade estrutural e funciona dos seres vivos'
+)
